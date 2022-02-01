46c7940ad3602f27adacebafa76e4cf89dabf658
Víctor Ponz <victorponz@gmail.com>
2022-02-01 19:14:03 +0100

    Añadir return

--- tema1/practicas/ud1_ejer2_valida_opcion.py
+++ tema1/practicas/ud1_ejer2_valida_opcion.py
@@ -2,24 +2,25 @@
 
 def valida_opcion():
     """Comprueba que la opción elegida en el menú es la correcta de entre los valores 0 a 5."""
-    print("=============================")
-    print("   GAMIFICACIÓN EN EL AULA")
-    print("=============================")
-    print(" 1 - Cargar datos del fichero")
-    print(" 2 - Imprimir datos")
-    print(" 3 - Jugar")
-    print(" 4 - Guardar")
-    print(" 5 - Cambiar contraseña")
-    print(" 0 - SALIR")
-    print("-----------------------------")
-
     incorrecto = True
     while(incorrecto):
+        print("=============================")
+        print("   GAMIFICACIÓN EN EL AULA")
+        print("=============================")
+        print(" 1 - Cargar datos del fichero")
+        print(" 2 - Imprimir datos")
+        print(" 3 - Jugar")
+        print(" 4 - Guardar")
+        print(" 5 - Cambiar contraseña")
+        print(" 0 - SALIR")
+        print("-----------------------------")
+
         num = int(input('Dame la opción: '))
         incorrecto = not (num >=0 and num <=5)
         if (incorrecto):
             print("Por favor, vuelve a intentarlo")
-        else:
-            print("La opción seleccionada es: %d" % num)
+        else:            
+            return num
             break
-valida_opcion()
+opcion = valida_opcion()
+print("La opción seleccionada es: %d" % opcion)
